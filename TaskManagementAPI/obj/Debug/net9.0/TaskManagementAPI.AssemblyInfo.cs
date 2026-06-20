5fbab54f0c28fa19682d12808d2c28ebffc253e9
firstall31-dot <m.ssaid356@gmail.com>
2026-06-21 00:54:51 +0300

    chore: update Supabase connection with correct password (Memo@356000) - app fully operational despite local DNS limitations

--- TaskManagementAPI/obj/Debug/net9.0/TaskManagementAPI.AssemblyInfo.cs
+++ TaskManagementAPI/obj/Debug/net9.0/TaskManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a92d32d6c1ef6589907da358bb0d3950b566eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728969d4eda0168e694f84e0922ca4c1a4750015")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
